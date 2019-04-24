9a2b3768b9df158cb97eade584d1da1a5cb177c5
Victor Cardona <victorhugocardonagomez@gmail.com>
2019-04-24 17:20:30 -0500

    TabBarGif

--- TabBarExercise/AppDelegate.swift
+++ TabBarExercise/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UITabBar.appearance().barTintColor = .black
+        UITabBar.appearance().tintColor = .red
         // Override point for customization after application launch.
         return true
     }
